f571c958e0128113dc4ca0714131b85904780e26
shoginyan <shoginyan@gmail.com>
2025-02-19 18:38:50 -0800

    Removed matchmaking bloat

--- nex/register_common_secure_server_protocols.go
+++ nex/register_common_secure_server_protocols.go
@@ -2,22 +2,10 @@ package nex
 
 import (
 	//"context"
-	"fmt"
-	nex "github.com/PretendoNetwork/nex-go/v2"
+
 	"github.com/PretendoNetwork/nex-go/v2/types"
-	common_globals "github.com/PretendoNetwork/nex-protocols-common-go/v2/globals"
-	common_match_making "github.com/PretendoNetwork/nex-protocols-common-go/v2/match-making"
-	common_match_making_ext "github.com/PretendoNetwork/nex-protocols-common-go/v2/match-making-ext"
-	common_matchmake_extension "github.com/PretendoNetwork/nex-protocols-common-go/v2/matchmake-extension"
-	common_nat_traversal "github.com/PretendoNetwork/nex-protocols-common-go/v2/nat-traversal"
 	common_secure "github.com/PretendoNetwork/nex-protocols-common-go/v2/secure-connection"
-	match_making "github.com/PretendoNetwork/nex-protocols-go/v2/match-making"
-	match_making_ext "github.com/PretendoNetwork/nex-protocols-go/v2/match-making-ext"
-	mm_types "github.com/PretendoNetwork/nex-protocols-go/v2/match-making/types"
-	matchmake_extension "github.com/PretendoNetwork/nex-protocols-go/v2/matchmake-extension"
-	nat_traversal "github.com/PretendoNetwork/nex-protocols-go/v2/nat-traversal"
 	secure "github.com/PretendoNetwork/nex-protocols-go/v2/secure-connection"
-	"github.com/PretendoNetwork/super-mario-3d-world/database"
 	local_globals "github.com/PretendoNetwork/super-mario-3d-world/globals"
 )
 
@@ -29,34 +17,4 @@ func registerCommonSecureServerProtocols() {
 		return nil
 	}
 
-	matchmakingManager := common_globals.NewMatchmakingManager(local_globals.SecureEndpoint, database.Postgres)
-
-	natTraversalProtocol := nat_traversal.NewProtocol()
-	local_globals.SecureEndpoint.RegisterServiceProtocol(natTraversalProtocol)
-	common_nat_traversal.NewCommonProtocol(natTraversalProtocol)
-
-	matchMakingProtocol := match_making.NewProtocol()
-	local_globals.SecureEndpoint.RegisterServiceProtocol(matchMakingProtocol)
-	commonMatchMakingProtocol := common_match_making.NewCommonProtocol(matchMakingProtocol)
-	commonMatchMakingProtocol.SetManager(matchmakingManager)
-
-	matchMakingExtProtocol := match_making_ext.NewProtocol()
-	local_globals.SecureEndpoint.RegisterServiceProtocol(matchMakingExtProtocol)
-	commonMatchMakingExtProtocol := common_match_making_ext.NewCommonProtocol(matchMakingExtProtocol)
-	commonMatchMakingExtProtocol.SetManager(matchmakingManager)
-
-	matchmakeExtensionProtocol := matchmake_extension.NewProtocol()
-	local_globals.SecureEndpoint.RegisterServiceProtocol(matchmakeExtensionProtocol)
-	commonMatchmakeExtensionProtocol := common_matchmake_extension.NewCommonProtocol(matchmakeExtensionProtocol)
-	commonMatchmakeExtensionProtocol.SetManager(matchmakingManager)
-
-	commonMatchmakeExtensionProtocol.CleanupSearchMatchmakeSession = func(matchmakeSession *mm_types.MatchmakeSession) {
-		//stubbed
-	}
-	commonMatchmakeExtensionProtocol.OnAfterAutoMatchmakeWithSearchCriteriaPostpone = func(packet nex.PacketInterface, lstSearchCriteria types.List[mm_types.MatchmakeSessionSearchCriteria], anyGathering types.AnyObjectHolder[mm_types.GatheringInterface], strMessage types.String) {
-		fmt.Println(anyGathering)
-	}
-	commonMatchmakeExtensionProtocol.CleanupMatchmakeSessionSearchCriterias = func(searchCriterias types.List[mm_types.MatchmakeSessionSearchCriteria]) {
-		//stubbed
-	}
 }
